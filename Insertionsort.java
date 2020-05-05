20c18ef1a46874346a4c92c79ce8b057e24c34cf
Rita <rita.ren.yuxi@gmail.com>
2020-05-04 23:37:54 -0700

    Update Insertionsort.java

--- Insertionsort.java
+++ Insertionsort.java
@@ -1,3 +1,9 @@
+//divide the array into two sbuarrays, the left one is sorted, the right one is unsorted. 
+//everytime taking the first item of the unsorted array as a marker, and put it at the correct position of the sorted array.
+//how to find the correct position? swap the marker with the bigger item.
+//repeating this process until the unsorted array is empty
+//Time: O(N^2)
+//Space: O(1)
 public class Insertionsort {
     public static int[] sort(int[] array) {
       if (array == null || array.length <= 1) {
