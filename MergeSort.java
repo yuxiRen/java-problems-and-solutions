8a49b25d6b31bcf7242bfd25fc8e2389d0671966
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-05 01:34:37 -0700

    format

--- MergeSort.java
+++ MergeSort.java
@@ -1,5 +1,5 @@
 public class MergeSort {
-    
+
     public int[] mergeSort(int[] array) {
         if (array == null || array.length < 2) {
             return array;
@@ -7,34 +7,34 @@ public class MergeSort {
         int[] helper = new int[array.length];
         mergeSort(array, 0, array.length - 1, helper);
         return array;
-  }
-  private void mergeSort(int[] array, int left, int right, int[] helper) {
-    if (left >= right) {
-      return;
     }
-    int mid = left + (right - left) / 2;
-    mergeSort(array, left, mid, helper);
-    mergeSort(array, mid + 1, right, helper);
-    merge(array, left, mid, right, helper);
-  }
 
-  private void merge(int[] array, int left, int mid, int right, int[] helper) {
-    //the merge operation is based on two sorted arrays
-    for (int i = left; i <= right; i++) {
-      helper[i] = array[i];
+    private void mergeSort(int[] array, int left, int right, int[] helper) {
+        if (left >= right) {
+            return;
+        }
+        int mid = left + (right - left) / 2;
+        mergeSort(array, left, mid, helper);
+        mergeSort(array, mid + 1, right, helper);
+        merge(array, left, mid, right, helper);
     }
-    int leftIdx = left;
-    int rightIdx = mid + 1;
-    while (leftIdx <= mid && rightIdx <= right) {
-      if (helper[leftIdx] < helper[rightIdx]) {
-        array[left++] = helper[leftIdx++];
-      } else {
-        array[left++] = helper[rightIdx++];
-      }
+
+    private void merge(int[] array, int left, int mid, int right, int[] helper) {
+        // the merge operation is based on two sorted arrays
+        for (int i = left; i <= right; i++) {
+            helper[i] = array[i];
+        }
+        int leftIdx = left;
+        int rightIdx = mid + 1;
+        while (leftIdx <= mid && rightIdx <= right) {
+            if (helper[leftIdx] < helper[rightIdx]) {
+                array[left++] = helper[leftIdx++];
+            } else {
+                array[left++] = helper[rightIdx++];
+            }
+        }
+        while (leftIdx <= mid) {
+            array[left++] = helper[leftIdx++];
+        }
     }
-    while (leftIdx <= mid) {
-      array[left++] = helper[leftIdx++];
-    }
-  }
-  }
-  
+}
