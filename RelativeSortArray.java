59dea2ff82214d400b1430fd2457cc3e052d90ae
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-05 23:23:38 -0700

    TreeMap

--- RelativeSortArray.java
+++ RelativeSortArray.java
@@ -19,4 +19,25 @@ public class RelativeSortArray {
         }
         return A1;
       }
+
+      public int[] sortSpecial(int[] A1, int[] A2) {
+        Map<Integer, Integer> map = new TreeMap<>();
+        for (int a1 : A1) {
+          map.put(a1, map.getOrDefault(a1, 0) + 1);
+        }
+        int i = 0;
+        for (int a2 : A2) {
+          while (map.get(a2) != null && map.get(a2) > 0) {
+            A1[i++] = a2;
+            map.put(a2, map.get(a2) - 1);
+          }
+        }
+        for (int n : map.keySet()) {
+          while (map.get(n) > 0) {
+            A1[i++] = n;
+            map.put(n, map.get(n) - 1);
+          }
+        }
+        return A1;
+      }
 }
