d3be40ecf777dcaae12f93995902d9e445435ef5
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-09 12:00:52 -0700

    add 2 methods

--- SearchAMatrix74.java
+++ SearchAMatrix74.java
@@ -1,5 +1,26 @@
 //method1: convert the 2D array to 1D array and do binary search
-//Time: O(N*M)
+//Time: O(log(N*M))
+
+public boolean searchMatrix(int[][] matrix, int target) {  
+    int row = matrix.length;
+    int col = matrix[0].length;
+    int start = 0;
+    int end = row * col - 1;
+    while (start < end) {
+        int mid = start + (end - start) / 2;
+        if (matrix[mid / col][mid % col] == target) {
+            return true;
+        } else if (matrix[mid / col][mid % col] < target) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    return matrix[start/col][start%col] == target;
+}   
+
+//method2: 
+//Time: O(logM + logN)
 public class SearchAMatrix74 {
     public boolean searchMatrix(int[][] matrix, int target) {
         if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) {
