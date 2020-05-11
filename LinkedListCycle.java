9793f323c7ce8b13d826d9bcabcaada55577da04
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-11 00:01:50 -0700

    add proof

--- LinkedListCycle.java
+++ LinkedListCycle.java
@@ -1,12 +1,21 @@
+/*
+prove: if there's a cycle, slow and fast must meet.
+proof by contradiction：if there's a cycle and slow and fast never meet.
+if fast at position i, slow at position i - 1
+but the last time fast at position i - 2, slow at position i-2
+they meet at the laast time.
+*/
+
 public class LinkedListCycle {
     if (head == null || head.next == null) {
         return false;
     }
     ListNode slow = head;
-    while (head != null && head.next != null) {
-        slow = slow.next;
-        head = head.next.next;
-        if (slow == head) {
+    ListNode fast = head;
+    while (fast != null && fast.next != null) {
+      slow = slow.next;
+      fast = fast.next.next;
+      if (slow == fast) {
             return true;
         }
     }
