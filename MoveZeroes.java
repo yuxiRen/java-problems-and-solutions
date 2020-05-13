045a990807201f2c8efbf7f3407256be6bd3d12f
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-13 00:29:57 -0700

    add common

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -17,12 +17,14 @@ public class MoveZeroes {
 
     //need to maintain the original array elements' order
     public int[] moveZero(int[] array) {
+        //subarray[0, end - 1] is the non-zero subarray
         int end = 0;
         for (int i = 0; i < array.length; i++) {
             if (array[i] != 0) {
                 array[end++] = array[i];
             }
         }
+        //fill in the right part with 0
         while (end < array.length) {
             array[end++] = 0;
         }
