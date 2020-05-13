50e765faabd0b1d64e932726230ba9ce6bc67e5b
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-13 00:28:59 -0700

    follow up

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,17 +1,31 @@
 public class MoveZeroes {
-    //if don't need to maintain the original array
+    // if don't need to maintain the original array elements' order
     public int[] moveZero(int[] array) {
         int i = 0;
         int j = array.length - 1;
         while (i < j) {
-          if (array[i] != 0) {
-            i++;
-          } else if (array[j] == 0) {
-            j--;
-          } else {
-            swap(array, i++, j--);
-          }
+            if (array[i] != 0) {
+                i++;
+            } else if (array[j] == 0) {
+                j--;
+            } else {
+                swap(array, i++, j--);
+            }
         }
         return array;
-      }
+    }
+
+    //need to maintain the original array elements' order
+    public int[] moveZero(int[] array) {
+        int end = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] != 0) {
+                array[end++] = array[i];
+            }
+        }
+        while (end < array.length) {
+            array[end++] = 0;
+        }
+        return array;
+    }
 }
