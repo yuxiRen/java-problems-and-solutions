6145c6c2f244f0ce90fa37e176b4b7fa89a31383
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-15 22:50:54 -0700

    leetcode 94

---
+++ BinaryTreeInorderTraversal.java
@@ -0,0 +1,22 @@
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while(root != null) {
+            stack.push(root);
+            root = root.left;
+        }
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+		//cur is the current traversing subtree’s root
+            res.add(cur.val);
+            cur = cur.right;
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+        }
+        return res;
+    }
+
+}
