335c9fc733b0ebd63866585baf64027c48dbd5a8
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-16 22:03:51 -0700

    leetcode 328

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,19 @@
+public class OddEvenLinkedList {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+          return head;
+        }
+        ListNode evenHead = head.next;
+        ListNode odd = head;
+        ListNode even = evenHead;
+        
+        while (even != null && even.next != null) {
+         odd.next = odd.next.next;
+         even.next = even.next.next;
+         odd = odd.next;
+         even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+      }
+}
