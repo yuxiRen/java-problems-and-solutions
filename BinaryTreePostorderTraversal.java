18a2bb3f653bce90d35a555392f8052d0fc431a0
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-17 22:11:57 -0700

    method2

--- BinaryTreePostorderTraversal.java
+++ BinaryTreePostorderTraversal.java
@@ -1,23 +1,56 @@
 public class BinaryTreePostorderTraversal {
+    // method1.
     public List<Integer> postOrder(TreeNode root) {
         List<Integer> res = new LinkedList<>();
         if (root == null) {
-          return res;
+            return res;
         }
-    
+
         Deque<TreeNode> stack = new ArrayDeque<>();
         stack.push(root);
         while (!stack.isEmpty()) {
-          TreeNode cur = stack.pop();
-          res.add(cur.key);
-          if (cur.left != null) {
-            stack.push(cur.left);
-          }
-          if (cur.right != null) {
-            stack.push(cur.right);
-          }
+            TreeNode cur = stack.pop();
+            res.add(cur.key);
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
         }
         Collections.reverse(res);
         return res;
-      }
+    }
+
+    // method2.
+    public List<Integer> postOrder(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        if (root == null) {
+            return res;
+        }
+
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        stack.push(root);
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.peek();
+            if (prev == null || prev.left == cur || prev.right == cur) {
+                if (cur.left != null) {
+                    stack.push(cur.left);
+                } else if (cur.right != null) {
+                    stack.push(cur.right);
+                } else {
+                    res.add(cur.key);
+                    stack.pop();
+                }
+            } else if (prev == cur.right || (prev == cur.left && cur.right == null)) {
+                res.add(cur.key);
+                stack.pop();
+            } else {
+                stack.push(cur.right);
+            }
+            prev = cur;
+        }
+        return res;
+    }
 }
