326f2182d54543e8b637c4f6bd1bcc8b32dbe8c2
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-18 00:27:38 -0700

    add problem

--- GetKeysInBSTInGivenRange.java
+++ GetKeysInBSTInGivenRange.java
@@ -1,3 +1,21 @@
+/*
+Get the list of keys in a given binary search tree in a given range[min, max] in ascending order, 
+both min and max are inclusive.
+
+Examples
+
+        5
+
+      /    \
+
+    3        8
+
+  /   \        \
+
+ 1     4        11
+
+get the keys in [2, 5] in ascending order, result is  [3, 4, 5]
+*/
 public class GetKeysInBSTInGivenRange {
     private List<Integer> res = new LinkedList<>();
     public List<Integer> getRange(TreeNode root, int min, int max) {
