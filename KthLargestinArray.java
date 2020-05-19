f51a1605a349d048cd49ff9e032f7f7105738d68
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-18 22:17:22 -0700

    add method2

--- KthLargestinArray.java
+++ KthLargestinArray.java
@@ -16,4 +16,39 @@ public class KthLargestinArray {
         }
         return minHeap.peek();
     }
+
+    //method2: Quick Select
+    public int findKthLargest(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left <= right) {
+            int idx = partition(nums, left, right);
+            if (idx == k - 1) {
+                return nums[idx];
+            } else if (idx < k - 1) {
+                left = idx + 1;
+            } else {
+                right = idx - 1;
+            }
+        }
+        return -1;
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = left + (right - left) * (int)Math.random();
+        swap(arr, pivot, left);
+        int l = left + 1;
+        int r = right;
+        while (l <= r) {
+            if (arr[l] > arr[left]) {
+                l++;
+            } else if (arr[r] < arr[left]) {
+                r--;
+            } else {
+                swap(arr, l++, r--);
+            }
+        }
+        swap(arr, left, r);
+        return r;
+    }
 }
