6268590d88a4fe6feefdf6498b0265c17407c1bd
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-18 22:43:20 -0700

    quick select

--- KSmallestInUnsortedArray.java
+++ KSmallestInUnsortedArray.java
@@ -28,4 +28,44 @@ public class KSmallestInUnsortedArray {
             System.out.print(r+", ");
         }
     }
+
+    //metohd2: quick select
+    public int[] kSmallest(int[] array, int k) {
+        int l = 0;
+        int r = array.length - 1;
+        while (l <= r) {
+            int idx = partition(array, l, r);
+            if (idx == k - 1) {
+                break;
+            } else if (idx < k - 1) {
+                l = idx + 1;
+            } else {
+                r = idx - 1;
+            }
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = array[i];
+        }
+        Arrays.sort(res);
+        return res;
+    }
+
+    private int partition(int[] arr, int left, int right) {
+        int pivot = left + (int)Math.random() * (right - left);
+        swap(arr, pivot, left);
+        int l = left + 1;
+        int r = right;
+        while (l <= r) {
+            if (arr[l] < arr[left]) {
+                l++;
+            } else if (arr[r] > arr[left]) {
+                r--;
+            } else {
+                swap(arr, l++, r--);
+            }
+        }
+        swap(arr, left, r);
+        return r;
+    }
 }
