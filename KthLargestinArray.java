8faa610a457b08232b2042472dc21c450fe8727e
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-18 22:01:58 -0700

    leetcode 215

---
+++ KthLargestinArray.java
@@ -0,0 +1,16 @@
+public class KthLargestinArray {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int n : nums) {
+            if (minHeap.size() < k) {
+                minHeap.offer(n);
+            } else {
+                if (n > minHeap.peek()) {
+                    minHeap.poll();
+                    minHeap.offer(n);
+                }
+            }
+        }
+        return minHeap.peek();
+    }
+}
