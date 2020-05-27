fc6f49611afb63a7bf07b746d63647e9321b68dc
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-27 00:37:11 -0700

    method3

--- MissingNumber.java
+++ MissingNumber.java
@@ -29,5 +29,18 @@ public class MissingNumber {
         }
         return sum - curSum;
     }
-
+    /*method3: bit
+    time: O(N)
+    space: O(1)
+    * */
+    public int missingNumber(int[] nums) {
+        int xor = 0;
+        for (int i = 0; i <= nums.length; i++) {
+            xor ^= i;
+        }
+        for (int n : nums) {
+            xor ^= n;
+        }
+        return xor;
+    }
 }
