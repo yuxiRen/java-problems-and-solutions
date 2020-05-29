527e109608792963bc90ce9b4bd73a3bcba4932b
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-28 23:57:42 -0700

    method2

--- RemoveAllAdjacentDuplicatesInString.java
+++ RemoveAllAdjacentDuplicatesInString.java
@@ -1,17 +1,30 @@
 public class RemoveAllAdjacentDuplicatesInString {
-    Deque<Character> stack = new ArrayDeque<>();
-        for(char s : S.toCharArray())    StringBuilder sb = new StringBuilder();
-
-{
-        if (!stack.isEmpty() && stack.peek() == s) {
-            stack.pop();
-        } else {
-            stack.push(s);
+    //method1:
+    public String removeDuplicates(String S) {
+        Deque<Character> stack = new ArrayDeque<>();
+        for (char s : S.toCharArray()) {
+            if (!stack.isEmpty() && stack.peek() == s) {
+                stack.pop();
+            } else {
+                stack.push(s);
+            }
         }
-    }
-        while (!stack.isEmpty())
-{
-        sb.append(stack.pop());
-    }
+        StringBuilder sb = new StringBuilder();
+        while (!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
         return sb.reverse().toString();
-}
+    }
+    //method2:
+    public String removeDuplicates (String S){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            if (sb.length() == 0 || sb.charAt(sb.length() - 1) != S.charAt(i)) {
+                sb.append(S.charAt(i));
+            } else {
+                sb.deleteCharAt(sb.length() - 1);
+            }
+        }
+        return sb.toString();
+    }
+    }
