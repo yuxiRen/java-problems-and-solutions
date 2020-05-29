92220ec5a664490e5f83a4ffa68b5cc17e27e202
yuxiRen <rita.ren.yuxi@gmail.com>
2020-05-28 22:00:26 -0700

    laicode 79

--- RemoveAdjacentRepeatedCharI.java
+++ RemoveAdjacentRepeatedCharI.java
@@ -1,4 +1,7 @@
+import java.util.Arrays;
+
 public class RemoveAdjacentRepeatedCharI {
+    //mthod1:
     public String deDup(String input) {
         char[] arr = input.toCharArray();
         int end = 0;
@@ -9,4 +12,18 @@ public class RemoveAdjacentRepeatedCharI {
         }
         return new String(arr, 0, end);
     }
+    //method2:
+    public String deDup(int[] array) {
+        if (array.length <= 1) {
+            return array;
+        }
+        int end = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] != array[end]) {
+                array[++end] = array[i];
+            }
+        }
+        return Arrays.copyOf(array, end + 1);
+    }
+
 }
