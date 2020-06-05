c184863bfeab6f69eb187733e61be09b76e5dd73
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-05 00:59:03 -0700

    iterative trversal

--- SpiralMatrix.java
+++ SpiralMatrix.java
@@ -35,4 +35,43 @@ public class SpiralMatrix {
         }
         recursiveTraverse(matrix, offset + 1, row - 2, col - 2, res);
     }
+    //method2. iterative trversal
+    public List<Integer> spiral(int[][] matrix) {
+        List<Integer> res = new ArrayList<>();
+        int left = 0;
+        int right = matrix[0].length - 1;
+        int up = 0;
+        int down = matrix.length - 1;
+        while (left < right && up < down) {
+            for (int i = left; i < right; i++) {
+                res.add(matrix[up][i]);
+            }
+            for (int i = up; i < down; i++) {
+                res.add(matrix[i][right]);
+            }
+            for (int i = right; i > left; i--) {
+                res.add(matrix[down][i]);
+            }
+            for (int i = down; i > left; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+        if (left > right || up > down) {
+            return res;
+        } else if (left == right) {
+            for (int i = up; i <= down; i++) {
+                res.add(matrix[i][left]);
+            }
+        } else {
+            for (int i = left; i <= right; i++) {
+                res.add(matrix[up][i]);
+            }
+        }
+
+        return res;
+    }
 }
