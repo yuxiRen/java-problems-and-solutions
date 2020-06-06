7eed0e4526d6098decb7ad7d06b7c78b7f33c7ea
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-05 23:37:30 -0700

    2 methods

--- ReverseLinkedListInPairs.java
+++ ReverseLinkedListInPairs.java
@@ -10,4 +10,18 @@ public class ReverseLinkedListInPairs {
         head.next = next;
         return newHead;
     }
+    //method2. iterative
+    public ListNode reverseInPairs(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            ListNode next = cur.next.next;
+            cur.next.next = next.next;
+            next.next = cur.next;
+            cur.next = next;
+            cur = cur.next.next;
+        }
+        return dummy.next;
+    }
 }
