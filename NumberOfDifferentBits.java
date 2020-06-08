d0bf02eeb2a9e7be6181f38b953c4a0f3d2ee90b
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-07 21:10:40 -0700

    laicode 75

---
+++ NumberOfDifferentBits.java
@@ -0,0 +1,12 @@
+//Determine the number of bits that are different for two given integers.
+public class NumberOfDifferentBits {
+    public int diffBits(int a, int b) {
+        a ^= b;
+        int count = 0;
+        while (a != 0) {
+            count += a & 1;
+            a >>>= 1;
+        }
+        return count;
+    }
+}
