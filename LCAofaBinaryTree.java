f2eab8d0c4e92cbbca0482533bdf63915f815b53
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-07 20:11:20 -0700

    leetcode 236

---
+++ LCAofaBinaryTree.java
@@ -0,0 +1,13 @@
+public class LCAofaBinaryTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode one, TreeNode two) {
+        if (root == null || root == one || root == two) {
+            return root;
+        }
+        TreeNode left = lowestCommonAncestor(root.left, one, two);
+        TreeNode right = lowestCommonAncestor(root.right, one,two);
+        if (left != null && right != null) {
+            return root;
+        }
+        return left == null ? right : left;
+    }
+}
