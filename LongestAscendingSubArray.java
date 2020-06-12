2c507fe1dbde0f126fbffce1b3fe6db532506ad0
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-11 22:08:22 -0700

    comments

--- LongestAscendingSubArray.java
+++ LongestAscendingSubArray.java
@@ -1,4 +1,14 @@
 public class LongestAscendingSubArray {
+    /*
+    base case:
+        M[0] = 1, when there is only one element in the array
+    state:
+ 	    M[i] represents: from the 0-th element to the i-th element(including),
+ 	    the length of the longest ascending subarray.
+    induction rule
+    M[i] = M[i - 1] + 1, if array[i] > array[i - 1]
+         = 1, else
+    */
     public int longest(int[] array) {
         if (array.length <= 1) {
             return array.length;
