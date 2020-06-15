165512ba988ce40f05d96bf8346509bc53414409
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-14 22:23:58 -0700

    comments

--- EditDistance.java
+++ EditDistance.java
@@ -1,4 +1,13 @@
 public class EditDistance {
+//    base case:
+//    M[0][0] = 0, there’s no string to edit
+//    state:
+//    M[i][j] represents the minimum steps to edit one.substring(0, i + 1) and two.substring(0, j + 1)
+//    induction rule:
+//    M[i][j] =
+//    case1. no need to edit: one.charAt(i) == two.charAt(j) →  M[i-1][j-1]
+//            case2. MIN(distance[i - 1][j - 1] + 1, distance[i][j - 1] + 1,distance[i - 1][j] + 1)
+
     public int editDistance(String one, String two) {
         int[][] distance = new int[one.length() + 1][two.length() + 1];
         for (int i = 0; i < distance.length; i++) {
