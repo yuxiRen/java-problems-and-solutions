75e39716450ea04ab159a98e4e80b7ec4911382c
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-24 00:05:26 -0700

    comments

--- LowestCommonAncestor.java
+++ LowestCommonAncestor.java
@@ -1,4 +1,8 @@
 public class LowestCommonAncestor {
+//    case1. there’s no node in the subtree → return null
+//    case2. there’s only one node in the subtree → return the node
+//    case3. there’re both two nodes in the subtree → return the root
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode one, TreeNode two) {
         if (root == null || root == one || root == two) {
             return root;
