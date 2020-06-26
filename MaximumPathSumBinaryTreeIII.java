2481684f8d8445a716a9633227bae43b5b29baa7
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-25 22:52:47 -0700

    laicode 140

--- MaximumPathSumBinaryTreeIII.java
+++ MaximumPathSumBinaryTreeIII.java
@@ -1,18 +1,16 @@
-public class BinaryTreeMaximumPathSum {
+public class MaximumPathSumBinaryTreeIII {
     private int max = Integer.MIN_VALUE;
-
     public int maxPathSum(TreeNode root) {
         maxPathSumHelper(root);
         return max;
     }
-
     private int maxPathSumHelper(TreeNode root) {
         if (root == null) {
             return 0;
         }
         int left = Math.max(0, maxPathSumHelper(root.left));
         int right = Math.max(0, maxPathSumHelper(root.right));
-        max = Math.max(max, root.key + left + right);
+        max = Math.max(max, root.key + Math.max(left, right));
         return root.key + Math.max(left, right);
     }
 }
