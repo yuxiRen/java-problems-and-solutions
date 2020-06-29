88c751eaaf5bdd69975fde4ad21b74f311a0f481
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-28 22:18:57 -0700

    leetcode 1008

---
+++ ConstructBinarySearchTreefromPreorderTraversal.java
@@ -0,0 +1,18 @@
+public class ConstructBinarySearchTreefromPreorderTraversal {
+    private int idx;
+    public TreeNode bstFromPreorder(int[] preorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        return bstFromPreorder(preorder, Integer.MAX_VALUE);
+    }
+    private TreeNode bstFromPreorder(int[] preorder, int parentVal) {
+        if (idx >= preorder.length || preorder[idx] >= parentVal) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[idx++]);
+        root.left = bstFromPreorder(preorder, root.val);
+        root.right = bstFromPreorder(preorder, parentVal);
+        return root;
+    }
+}
