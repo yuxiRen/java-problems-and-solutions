b961fdbd2f1cd6b396d0c3b0f64d13f9838dcf8a
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-29 23:32:13 -0700

    2methods

--- BinaryTreeUpsideDown.java
+++ BinaryTreeUpsideDown.java
@@ -1,4 +1,5 @@
 public class BinaryTreeUpsideDown {
+    //method1. recursion
     public TreeNode reverse(TreeNode root) {
         if (root == null || root.left == null) {
             return root;
@@ -9,5 +10,20 @@ public class BinaryTreeUpsideDown {
         root.left = null;
         root.right = null;
         return newRoot;
+    }
+    //method2. iteration
+    public TreeNode reverse(TreeNode root) {
+        TreeNode prevRoot = null;
+        TreeNode prevRight = null;
+        while (root != null) {
+            TreeNode left = root.left;
+            TreeNode right = root.right;
+            root.left = prevRoot;
+            root.right = prevRight;
+            prevRoot = root;
+            root = left;
+            prevRight = right;
+        }
+        return prevRoot;
     }
 }
