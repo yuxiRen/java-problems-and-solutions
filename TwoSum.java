85465a9cfa53af9bc4a724345806c0564b42db2e
yuxiRen <rita.ren.yuxi@gmail.com>
2020-06-30 21:52:25 -0700

    leetcode 1

---
+++ TwoSum.java
@@ -0,0 +1,16 @@
+public class TwoSum {
+    //unsorted array
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                res[0] = i;
+                res[1] = map.get(target - nums[i]);
+                return res;
+            }
+            map.put(nums[i], i);
+        }
+        return res;
+    }
+}
