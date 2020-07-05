38e5c1ed93bf2cba3ff4a4b1b2098cc58e9bcfd0
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-05 01:46:14 -0700

    2 methods

--- UglyNumberII.java
+++ UglyNumberII.java
@@ -1,4 +1,5 @@
 public class UglyNumberII {
+    //method1. time: O(nlogn)  space: O(n)
     public int nthUglyNumber(int n) {
         PriorityQueue<Integer> minHeap = new PriorityQueue<>();
         minHeap.offer(1);
@@ -17,5 +18,27 @@ public class UglyNumberII {
             n--;
         }
         return minHeap.peek();
+    }
+    //method2. time: O(n) space: O(1)
+    public int nthUglyNumber(int n) {
+        int i2 = 0;
+        int i3 = 0;
+        int i5 = 0;
+        int[] q = new int[n];
+        q[0] = 1;
+        for (int i = 1; i < n; i++) {
+            int num = Math.min(q[i2] * 2, Math.min(q[i3] * 3, q[i5] * 5));
+            if (num == q[i2] * 2) {
+                i2++;
+            }
+            if (num == q[i3] * 3) {
+                i3++;
+            }
+            if (num == q[i5] * 5) {
+                i5++;
+            }
+            q[i] = num;
+        }
+        return q[n - 1];
     }
 }
