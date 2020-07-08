310840a9b6ca6a567e1453abb71feaef28199600
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-07 23:55:03 -0700

    2 methods

--- KthSmallestInTwoSortedArrays.java
+++ KthSmallestInTwoSortedArrays.java
@@ -19,5 +19,27 @@ public class KthSmallestInTwoSortedArrays {
             return a[i] < b[j] ? a[i] : b[j];
         }
     }
+    //method2 time: O(log(a+b))
+    public int kth(int[] a, int[] b, int k) {
+        return kth(a, 0, b, 0, k);
+    }
+    private int kth(int[] a, int aStart, int[] b, int bStart, int k) {
+        if (aStart >= a.length) {
+            return b[bStart + k -1];
+        }
+        if (bStart >= b.length) {
+            return a[aStart + k -1];
+        }
+        if (k == 1) {
+            return a[aStart] < b[bStart] ? a[aStart] : b[bStart];
+        }
+        int aMid = aStart + k / 2 - 1;
+        int bMid = bStart + k / 2 - 1;
+        if (aMid >= a.length || (bMid < b.length && a[aMid] > b[bMid])) {
+            return kth(a, aStart, b, bMid + 1, k - k / 2);
+        } else {
+            return kth(a, aMid + 1, b, bStart, k - k / 2);
+        }
+    }
 }
 
