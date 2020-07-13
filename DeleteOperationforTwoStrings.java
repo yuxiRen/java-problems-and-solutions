7db2b626d190e12b464714d69c931d79bc904558
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-12 21:23:30 -0700

    leetcode 583

---
+++ DeleteOperationforTwoStrings.java
@@ -0,0 +1,26 @@
+public class DeleteOperationforTwoStrings {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        word1 = "#" + word1;
+        word2 = "#" + word2;
+        int dp[][] = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            dp[i][0] = i;
+        }
+        for (int j = 0; j <= n; j++) {
+            dp[0][j] = j;
+        }
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i) == word2.charAt(j)) {
+                    dp[i][j] = dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i - 1][j] + 1, dp[i][j - 1] + 1);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
