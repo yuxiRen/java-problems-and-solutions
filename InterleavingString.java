00a35db5cb3c39c2b2b11d973c0c9b9ae11a57c2
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-12 22:23:01 -0700

    rewrite basecases

--- InterleavingString.java
+++ InterleavingString.java
@@ -10,17 +10,16 @@ public class InterleavingString {
         s3 = "#" + s3;
         boolean[][] dp = new boolean[len1 + 1][len2 + 1];
 
-        for (int i = 0; i <= len1; i++) {
-            for (int j = 0; j <= len2; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = true;
-                } else if (i == 0) {
-                    dp[i][j] = s2.charAt(j) == s3.charAt(i + j) && dp[i][j - 1];
-                } else if (j == 0) {
-                    dp[i][j] = s1.charAt(i) == s3.charAt(i + j) && dp[i - 1][j];
-                } else {
-                    dp[i][j] = (dp[i - 1][j] && s1.charAt(i) == s3.charAt(i + j)) || (dp[i][j - 1] && s2.charAt(j) == s3.charAt(i + j));
-                }
+        dp[0][0] = true;
+        for (int i = 1; i <= len1; i++) {
+            dp[i][0] = s1.charAt(i) == s3.charAt(i) && dp[i - 1][0];
+        }
+        for (int i = 1; i <= len2; i++) {
+            dp[0][i] = s2.charAt(i) == s3.charAt(i) && dp[0][i - 1];
+        }
+        for (int i = 1; i <= len1; i++) {
+            for (int j = 1; j <= len2; j++) {
+                dp[i][j] = (dp[i - 1][j] && s1.charAt(i) == s3.charAt(i + j)) || (dp[i][j - 1] && s2.charAt(j) == s3.charAt(i + j));
             }
         }
 
