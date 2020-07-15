ec3d4b0e2727fe93a8034e9736ebc24c3d55854f
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-14 22:49:34 -0700

    set final

--- HouseRobber.java
+++ HouseRobber.java
@@ -3,15 +3,16 @@ public class HouseRobber {
         if (nums == null || nums.length == 0) {
             return 0;
         }
+        int norob = 0;
+        int rob = 1;
         int n = nums.length;
         int[][] dp = new int[n][2];
-        dp[0][1] = nums[0];
-        for (int i = 1; i < nums.length; i++) {
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
-            dp[i][1] = dp[i - 1][0] + nums[i];
+        dp[0][rob] = nums[0];
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = Math.max(dp[i - 1][norob], dp[i - 1][rob]);
+            dp[i][rob] = dp[i - 1][norob] + nums[i];
         }
-        return dp[n - 1][0] > dp[n - 1][1] ? dp[n - 1][0] : dp[n - 1][1];
+        return Math.max(dp[n - 1][norob], dp[n - 1][rob]);
     }
-
 }
 
