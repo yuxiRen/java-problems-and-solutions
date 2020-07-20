e59f9a2c658226553d95dbfe5a15e3d8982c3f5f
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-19 22:23:18 -0700

    rewrite

--- MergeIntervals.java
+++ MergeIntervals.java
@@ -1,29 +1,26 @@
 public class MergeIntervals {
     //method1
     public int[][] merge(int[][] intervals) {
-        if (intervals.length <= 1) {
-            return intervals;
-        }
         List<int[]> result = new ArrayList<>();
-        List<Point> points = new ArrayList<>();
+        List<int[]> points = new ArrayList<>();
         for (int[] interval : intervals) {
-            points.add(new Point(interval[0], 1));
-            points.add(new Point(interval[1], -1));
+            points.add(new int[]{interval[0], 1});
+            points.add(new int[]{interval[1], -1});
         }
         Collections.sort(points, (p1, p2) -> {
-            if (p1.position == p2.position) {
-                return p2.flag - p1.flag;
+            if (p1[0] == p2[0]) {
+                return p2[1] - p1[1];
             }
-            return p1.position - p2.position;
+            return p1[0] - p2[0];
         });
-        int start = points.get(0).position;
+        int start = points.get(0)[0];
         int count = 0;
         for (int i = 0; i < points.size(); i++) {
-            count += points.get(i).flag;
+            count += points.get(i)[1];
             if (count == 0) {
-                result.add(new int[]{start, points.get(i).position});
+                result.add(new int[]{start, points.get(i)[0]});
                 if (i < points.size() - 1) {
-                    start = points.get(i + 1).position;
+                    start = points.get(i + 1)[0];
                 }
             }
         }
@@ -54,13 +51,4 @@ public class MergeIntervals {
         return result.toArray(new int[result.size()][]);
     }
 
-    class Point {
-        int position;
-        int flag;
-
-        Point(int p, int f) {
-            this.position = p;
-            this.flag = f;
-        }
-    }
 }
