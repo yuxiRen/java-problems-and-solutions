d4683029455023650d627092b8a0d67f570988c6
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-21 00:27:10 -0700

    method2 for the follow up

--- ClosestBinarySearchTreeValueII.java
+++ ClosestBinarySearchTreeValueII.java
@@ -19,4 +19,36 @@ public class ClosestBinarySearchTreeValueII {
         res.add(root.val);
         helper(res, root.right);
     }
+
+    //method2
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> res = new LinkedList<>();
+        Deque<TreeNode> predecessors = new ArrayDeque<>();
+        Deque<TreeNode> successors = new ArrayDeque<>();
+        inorder(root, target, predecessors, true);
+        inorder(root, target, successors, false);
+        while (res.size() < k) {
+            if (predecessors.isEmpty()) {
+                res.add(successors.pop().val);
+            } else if (successors.isEmpty()) {
+                res.add(predecessors.pop().val);
+            } else if (Math.abs(predecessors.peek().val - target) < Math.abs(successors.peek().val - target)) {
+                res.add(predecessors.pop().val);
+            } else {
+                res.add(successors.pop().val);
+            }
+        }
+        return res;
+    }
+
+    private void inorder(TreeNode root, double target, Deque<TreeNode> stack, boolean isPredecessors) {
+        if (root == null) {
+            return;
+        }
+        inorder(isPredecessors ? root.left : root.right, target, stack, isPredecessors);
+        if ((isPredecessors && root.val <= target) || (!isPredecessors && root.val > target)) {
+            stack.push(root);
+            inorder(isPredecessors ? root.right : root.left, target, stack, isPredecessors);
+        }
+    }
 }
