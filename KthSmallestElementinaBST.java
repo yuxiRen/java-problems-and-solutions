8c0f9793b9a7f3d0e4a79fe9ee49b4ba58f65c2b
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-22 23:38:16 -0700

    leetcode 230

---
+++ KthSmallestElementinaBST.java
@@ -0,0 +1,19 @@
+public class KthSmallestElementinaBST {
+    public int kthSmallest(TreeNode root, int k) {
+        Deque<TreeNode> stack = new ArrayDeque<>();
+        while(root != null || !stack.isEmpty()) {
+            if (root != null) {
+                stack.push(root);
+                root = root.left;
+            } else {
+                root = stack.pop();
+                if (--k == 0) {
+                    return root.val;
+                }
+
+                root = root.right;
+            }
+        }
+        return -1;
+    }
+}
