d5d48eac763189476436f8bfbc36bc10e8668e5f
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-29 21:50:09 -0700

    rewrite

--- ConvertSortedArraytoBinarySearchTree.java
+++ ConvertSortedArraytoBinarySearchTree.java
@@ -1,14 +1,14 @@
 public class ConvertSortedArraytoBinarySearchTree {
     public TreeNode sortedArrayToBST(int[] nums) {
-        return sortedArrayToBST(nums, 0, nums.length);
+        return sortedArrayToBST(nums, 0, nums.length - 1);
     }
     private  TreeNode sortedArrayToBST(int[] nums, int start, int end) {
-        if (start == end) {
+        if (start > end) {
             return null;
         }
         int mid = start + (end - start) / 2;
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = sortedArrayToBST(nums, start, mid);
+        root.left = sortedArrayToBST(nums, start, mid - 1);
         root.right = sortedArrayToBST(nums, mid + 1, end);
         return root;
     }
