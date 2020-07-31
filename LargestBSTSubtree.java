83c59844d9f1cbd7080c09bd9ac1ab64f6e26e4d
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-31 00:40:52 -0700

    method2

--- LargestBSTSubtree.java
+++ LargestBSTSubtree.java
@@ -24,4 +24,20 @@ public class LargestBSTSubtree {
         }
         return count(root.left) + count(root.right) + 1;
     }
+    //method2. O(N)
+    public int largestBSTSubtree(TreeNode root) {
+        return helper(root)[2];
+    }
+    private int[] helper(TreeNode root) {
+        if (root == null) {
+            return new int[]{Integer.MAX_VALUE, Integer.MIN_VALUE, 0};
+        }
+        int[] left = helper(root.left);
+        int[] right = helper(root.right);
+        if (root.val > left[1] && root.val < right[0]) {
+            return new int[]{Math.min(root.val, left[0]), Math.max(root.val, right[1]), left[2] + right[2] + 1};
+        } else {
+            return new int[]{Integer.MIN_VALUE, Integer.MAX_VALUE, Math.max(left[2], right[2])};
+        }
+    }
 }
