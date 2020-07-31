fd5b4c17eb86284a8184993c9694d6b4b183568e
yuxiRen <rita.ren.yuxi@gmail.com>
2020-07-30 23:17:17 -0700

    method2

--- VerifyPreorderSequenceinBinarySearchTree.java
+++ VerifyPreorderSequenceinBinarySearchTree.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class VerifyPreorderSequenceinBinarySearchTree {
     //method1. DFS O(NlogN)
     public boolean verifyPreorder(int[] preorder) {
@@ -18,5 +20,26 @@ public class VerifyPreorderSequenceinBinarySearchTree {
             }
         }
         return verifyPreorder(preorder, start + 1, i - 1) && verifyPreorder(preorder, i, end);
+    }
+    //method2.
+    public static boolean verifyPreorder(int[] preorder) {
+        //descending stack
+        Stack<Integer> stack = new Stack<>();
+
+        int min = Integer.MIN_VALUE;
+        for (int num : preorder) {
+            //if the right subtree bigger than root, return false
+            if (num < min) {
+                return false;
+            }
+            //reach the right subtree
+            while (!stack.isEmpty() && num > stack.peek()) {
+                //pop all the leftsubtree & root
+                //pick the root as the new boundary
+                min = stack.pop();
+            }
+            stack.push(num);
+        }
+        return true;
     }
 }
