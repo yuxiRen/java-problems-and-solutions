9daffb97e8de12ba790e0690682ea46c1b55f0bb
yuxiRen <rita.ren.yuxi@gmail.com>
2020-08-01 23:57:43 -0700

    method2

--- CourseSchedule.java
+++ CourseSchedule.java
@@ -37,4 +37,39 @@ public class CourseSchedule {
         visited[cur] = 1;
         return true;
     }
+    //method2. BFS
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        Map<Integer, List<Integer>> graph = new HashMap<Integer, List<Integer>>();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> queue = new ArrayDeque<>();
+        int count = 0;
+        for (int[] p : prerequisites) {
+            if (!graph.containsKey(p[0])) {
+                graph.put(p[0], new ArrayList<Integer>());
+            }
+            graph.get(p[0]).add(p[1]);
+            indegree[p[1]]++;
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            count++;
+            if (graph.containsKey(cur)) {
+                List<Integer> prev = graph.get(cur);
+                for (int p : prev) {
+                    indegree[p]--;
+                    if (indegree[p] == 0) {
+                        queue.offer(p);
+                    }
+                }
+            }
+        }
+
+        return count == numCourses;
+    }
 }
