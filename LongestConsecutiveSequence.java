631ed8ec2bea98fd691f4e1cf8bbf1695a79d147
yuxiRen <rita.ren.yuxi@gmail.com>
2020-08-12 21:40:51 -0700

    leetcode 128

---
+++ LongestConsecutiveSequence.java
@@ -0,0 +1,45 @@
+public class LongestConsecutiveSequence {
+    public int[] parent;
+    public int[] size;
+    public int longestConsecutive(int[] nums) {
+        int n = nums.length;
+        parent = new int[n];
+        size = new int[n];
+        //<value, idx>
+        Map<Integer, Integer> idx = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            if (idx.containsKey(nums[i])) {
+                continue;
+            }
+            parent[i] = i;
+            size[i] = 1;
+            if (idx.containsKey(nums[i] - 1)) {
+                union(i, idx.get(nums[i] - 1));
+            }
+            if (idx.containsKey(nums[i] + 1)) {
+                union(i, idx.get(nums[i] + 1));
+            }
+            idx.put(nums[i], i);
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            res = Math.max(size[i], res);
+        }
+        return res;
+    }
+    private int find(int x) {
+        if (x != parent[x]) {
+            parent[x] = find(parent[x]);
+        }
+        return parent[x];
+    }
+    private void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            parent[rootX] = rootY;
+            size[find(rootY)] += size[rootX];
+        }
+    }
+}
+
